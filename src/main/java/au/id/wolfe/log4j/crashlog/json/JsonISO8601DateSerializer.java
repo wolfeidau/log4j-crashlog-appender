fffded9141099c819f8c8b0fb83e55181f149bf8
Mark Wolfe <mark@wolfe.id.au>
2012-08-07 17:30:42 +1000

    Fixes for date format and some tweaks to testing.

---
+++ src/main/java/au/id/wolfe/log4j/crashlog/json/JsonISO8601DateSerializer.java
@@ -0,0 +1,11 @@
+package au.id.wolfe.log4j.crashlog.json;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: markw
+ * Date: 7/08/12
+ * Time: 4:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JsonISO8601DateSerializer {
+}
